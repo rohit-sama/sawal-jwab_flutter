094efab6b63481dda8c2b9105c5fc197e1c7a6ae
rohit-sama <rohit9804singh@gmail.com>
2023-06-21 01:14:04 +0530

    Revert "changes"
    This reverts commit e771d74603e79574dd2467fc5a639e0bad341d3f.

--- lib/main.dart
+++ lib/main.dart
@@ -32,83 +32,22 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
 
 List<Widget> scoreKeeper = [];
-List<String> questions = [
-  "do you watch anime?",
-  "have you seen death note?",
-  "have you seen attack on titan?",
-  "have you seen demon slayer?",
-  "have you seen jujutsu kaisen",
-  "have you seen naruto?",
-  "have you seen bleach?",
-  "have you seen one punch man?",
-  "have you seen fullmetal alchemist?",
-  "and finally, have you seen one piece?"
-];
-int count = 0;
-int trueCount = 0;
-int falseCount = 0;
+
   @override
   Widget build(BuildContext context) {
-    if(count  == 10 && trueCount < 11 && trueCount > 6) {
-      return Center(
-        child: Column(
-            children: [
-              Container(
-                height: 370.0,
-              ),
-                Expanded(
-                child: Center(
-                  child: Text("Congrats !! you scored $trueCount/10"  ,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                  ),),
-                ),
-              ),
-              Expanded(
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                      onPressed: () {
-                        count =0;
-                        trueCount=0;
-                        falseCount=0;
-                        scoreKeeper.clear();
-                        setState(() {});
-                      },
-                      child: const Text(
-                        'GO BACK',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                height: 320.0,
-              ),
-            ],
-          ),
-      );
-    }
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-         Expanded(
+        const Expanded(
           flex: 5,
           child: Padding(
-            padding: const EdgeInsets.all(10.0),
+            padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[count],
+                'This is where the question text will go.',
                 textAlign: TextAlign.center,
-                style: const TextStyle(
+                style: TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -122,8 +61,6 @@ int falseCount = 0;
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
               onPressed: () {
-                count ++;
-                trueCount++;
                 setState(() {
                   scoreKeeper.add(
                     const Icon(
@@ -148,8 +85,6 @@ int falseCount = 0;
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent),
               onPressed: () {
-                count ++;
-                falseCount++;
                 setState(() {
                   scoreKeeper.add(
                     const Icon(
