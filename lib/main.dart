9f82bfd3111d20b1d3f20e951cf2c0ad4ebdb6cb
rohit-sama <rohit9804singh@gmail.com>
2023-06-21 02:32:41 +0530

    Merge remote-tracking branch 'origin/master'

--- lib/main.dart
+++ lib/main.dart
@@ -31,27 +31,150 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
 
-List<Widget> scoreKeeper = [];
-
+  List<Widget> scoreKeeper = [];
+  List<String> questions = [
+    "do you watch anime?",
+    "have you seen death note?",
+    "have you seen attack on titan?",
+    "have you seen demon slayer?",
+    "have you seen jujutsu kaisen",
+    "have you seen naruto?",
+    "have you seen bleach?",
+    "have you seen one punch man?",
+    "have you seen fullmetal alchemist?",
+    "and finally, have you seen one piece?"
+  ];
+  int count = 0;
+  int trueCount = 0;
+  int falseCount = 0;
+  
   @override
   Widget build(BuildContext context) {
+    if(count  == 10 && trueCount < 11 && trueCount > 6) {
+      return Center(
+        child: Column(
+          children: [
+            Container(
+              height: 200.0,
+            ),
+            Expanded(
+              child: Center(
+                child: Column(
+                  children: [
+                    Image.asset('assets/anime-congratulations.gif'),
+                    Text("Congrats, you are a weeb !! you scored $trueCount/10"  ,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                    onPressed: () {
+                      count =0;
+                      trueCount=0;
+                      falseCount=0;
+                      scoreKeeper.clear();
+                      setState(() {});
+                    },
+                    child: const Text(
+                      'GO BACK',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 150.0,
+            ),
+          ],
+        ),
+      );
+    }
+    if(count == 10 && trueCount < 7 ) {
+      return Center(
+        child: Container(
+              child: Column(
+                children: [
+                  Container(
+                    height: 180,
+                  ),
+                  Image.asset('assets/anime-noob.gif'),
+                   const Text("you need to work harder man !!"  ,
+                     style: TextStyle(
+                       color: Colors.white,
+                       fontSize: 20.0,
+                     ),),
+                  Expanded(
+                    child: Center(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                          onPressed: () {
+                            count =0;
+                            trueCount=0;
+                            falseCount=0;
+                            scoreKeeper.clear();
+                            setState(() {});
+                          },
+                          child: const Text(
+                            'GO BACK',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  Container(
+                    height: 200,
+                  ),
+                ],
+              ),
+            ),
+      );
+      
+    }
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        const Expanded(
+
+        Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 100.0,
                 ),
-              ),
+                Image.asset('assets/anuime.jpg'),
+                Container(
+                  height: 10.0,
+                ),
+                Center(
+                  child: Text(
+                    questions[count],
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
@@ -61,6 +184,8 @@ List<Widget> scoreKeeper = [];
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
               onPressed: () {
+                count ++;
+                trueCount++;
                 setState(() {
                   scoreKeeper.add(
                     const Icon(
@@ -85,6 +210,8 @@ List<Widget> scoreKeeper = [];
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent),
               onPressed: () {
+                count ++;
+                falseCount++;
                 setState(() {
                   scoreKeeper.add(
                     const Icon(
@@ -102,6 +229,9 @@ List<Widget> scoreKeeper = [];
               ),
             ),
           ),
+        ),
+        Container(
+          height: 10.0,
         ),
         Row(
           children: scoreKeeper,
